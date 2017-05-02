c89feaa5d42b4247c0cfadfaf60d485e21dfd13b
6yang <tangshuixiaoer@163.com>
2017-05-02 21:03:03 +0800

    脸谱

---
+++ 脸谱/js/act.js
@@ -0,0 +1,17 @@
+
+		$(function(){
+		
+			$.fn.fullpage({
+		
+				slidesColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', '#f90'],
+		
+				anchors: ['page1', 'page2', 'page3', 'page4','page5','page6'],
+		
+				
+				menu: '#menu',
+				
+				verticalCentered:true
+			});
+			
+		
+		});
